aac242b10762b59d24254c9bdfaba26ce5da6ed3
SafnasKaldeen <safnaskm.20@uom.lk>
2026-07-17 18:45:15 +0530

    af

--- next.config.js
+++ next.config.js
@@ -14,7 +14,12 @@ const nextConfig = {
             {
                 protocol: "https",
                 hostname: "crystalpng.com",
+            },
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",   
             }
+
         ],
     },
 };
